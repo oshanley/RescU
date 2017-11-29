8c84fa061d9e14aa6b1516bac43ad60be0494df6
Olivia Shanley <olivia.shanley@gmail.com>
2017-11-29 06:07:01 +0000

    Fixed bug where update threw null user error

--- rescu/app/controllers/reports_controller.rb
+++ rescu/app/controllers/reports_controller.rb
@@ -1,5 +1,5 @@
 class ReportsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :edit, :search, :destroy]
+  before_action :logged_in_user, only: [:create, :update, :destroy]
  
   def index
     if params[:location].present?

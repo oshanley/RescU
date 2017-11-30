9eb784e45f9373d7aa515de9e42a3942e64344b1
Olivia Shanley <olivia.shanley@gmail.com>
2017-11-30 18:50:19 -0500

    Update application_helper.rb

--- rescu/app/helpers/application_helper.rb
+++ rescu/app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
+=begin
+Name: Olivia Shanley
+Course: CSC 415
+Semester: Fall 2017
+Instructor: Dr. Pulimood 
+Project name: RescU 
+Description: An platform to help facilitate animal rescues
+Filename: application_helper.rb
+Description: Helpers for the Application
+=end
+
 module ApplicationHelper
 
+  # Returns flash notice class type for Bootstrap styling
   def flash_class(level)
     return case level
         when "notice"; "alert alert-info"

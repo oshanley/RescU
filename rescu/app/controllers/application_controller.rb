2a4132b2c77e66a87b4b594a2d5762fcea48e83d
Olivia Shanley <olivia.shanley@gmail.com>
2017-11-30 18:46:02 -0500

    Update application_controller.rb

--- rescu/app/controllers/application_controller.rb
+++ rescu/app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
+=begin
+Name: Olivia Shanley
+Course: CSC 415
+Semester: Fall 2017
+Instructor: Dr. Pulimood 
+Project name: RescU 
+Description: An platform to help facilitate animal rescues
+Filename: application_controller.rb
+Description: Controller for the Application
+=end
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   helper_method :current_user, :flash_class
